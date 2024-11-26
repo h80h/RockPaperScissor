49e96a08b4bc97b79b71e4ad199c9fd467a59661
h80h <h80h525@gmail.com>
2024-11-26 11:30:26 +0800

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let randomNum;
+
 function getComputerChoice(){
-  // use Math.random() to ramdomly pick up number 1 - 3
+  // use Math.floor(Math.random()) to ramdomly pick up number 1 - 3
   // if it picks 1, return 'rock'
   // if it picks 2, return 'paper'
   // if it picks 3, return 'scissors' 
+  randomNum = Math.floor((Math.random() * 3)) + 1
+  switch (randomNum){
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
 }
