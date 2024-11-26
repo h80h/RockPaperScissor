01b657d803b38eb08af678751c08007a66eb679a
h80h <h80h525@gmail.com>
2024-11-26 11:09:43 +0800

    Write some pseudocode for script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+function getComputerChoice(){
+  // use Math.random() to ramdomly pick up number 1 - 3
+  // if it picks 1, return 'rock'
+  // if it picks 2, return 'paper'
+  // if it picks 3, return 'scissors' 
+}
