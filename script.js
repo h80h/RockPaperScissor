ba26fa78c3401d8dcff9341a7596fdc166475582
h80h <h80h525@gmail.com>
2024-11-27 13:53:59 +0800

    Revise the logic of this game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let userRPS;
+let randomNum;
 let humanScore = 0;
 let computerScore = 0;
 let draw = 0;
@@ -12,42 +12,41 @@ playGame();
 
 function getComputerChoice(){
   // use Math.floor(Math.random()) to ramdomly pick up number 1 - 3
-  return Math.floor((Math.random() * 3)) + 1;
+  randomNum = Math.floor((Math.random() * 3)) + 1;
+  switch(randomNum){
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors"
+  }
 }
 
 function getHumanChoice(){
   // use prompt() to let and get user's input
   // give user hint message in pop up dialogue
   // need to declare a variable to store the input!
-  userRPS = prompt("rock / paper / scissors ?").toLowerCase();
-  
-  return userRPSConvert();
-
-  function userRPSConvert(){
-    switch (userRPS){
-      case "rock":
-        return 1;
-      case "paper":
-        return 2;
-      case "scissors":
-        return 3;
-    }
-  }
+  return prompt("rock / paper / scissors ?").toLowerCase();
 }
 
 function playGame(){
   // Use for loop to make this game run 5 rounds
   // What needs to repeat are: getHumanChoice(), getComputerChoice(), playRound()
   
+  alert("Let's do a total of 5 rounds 'Rock, Paper, Scissors' with the computer! 👻 ")
+  
   for(let i = 0 ; i < 5 ; i++){
+    alert(`Round ${i+1}`);
     humanSelection = getHumanChoice();
     computerSelection = getComputerChoice();
 
+    console.log(`Round ${i+1}:`)
     playRound(humanSelection, computerSelection);
   }
 
   // Announce the winner
-  if (humanScore > computerScore){
+  if (humanScore > computerScore ){
     console.log("The final winner is...YOU! Congrats! 🎉")
   } else if(humanScore < computerScore){
     console.log("What?? It's the computer beats you at the end...👾")
@@ -60,42 +59,33 @@ function playGame(){
     // ELSE IF computerChoice is the winner, increase computerScore by 1
     // Here's the table to show whether human or computer win (a is human):
     //
-    // +=====+========+========+========+
-    // | b\a |   1    |   2    |   3    |
-    // +=====+========+========+========+
-    // |   1 | draw   | a(win) | a(win) |
-    // +-----+--------+--------+--------+
-    // |   2 | b(win) | draw   | a(win) |
-    // +-----+--------+--------+--------+
-    // |   3 | b(win) | b(win) | draw   |
-    // +-----+--------+--------+--------+
-    
-    const computerRPS = computerChoiceConvert();
-    
-    if(humanChoice > computerChoice){
+    // +==========+========+========+==========+
+    // |   b\a    |  rock  | paper  | scissors |
+    // +==========+========+========+==========+
+    // | rock     | draw   | a(win) | b(win)   |
+    // +----------+--------+--------+----------+
+    // | paper    | b(win) | draw   | a(win)   |
+    // +----------+--------+--------+----------+
+    // | scissors | a(win) | b(win) | draw     |
+    // +----------+--------+--------+----------+
+
+    console.log(`You choose ${humanSelection}; Computer choose ${computerSelection}`)
+
+    if((humanChoice == "rock" && computerChoice == "scissors") ||
+      (humanChoice == "paper" && computerChoice == "rock") ||
+      (humanChoice == "scissors" && computerChoice == "paper")){
       humanScore ++;
-      console.log(`You win, ${userRPS} beats ${computerRPS}`);
-    }else if(humanChoice < computerChoice){
-      computerScore ++;
-      console.log(`You lose...${userRPS} is beaten by ${computerRPS}`);
+      console.log(`You win, ${humanSelection} beats ${computerSelection}`);
     }else if(humanChoice == computerChoice){
       draw ++;
-      console.log(`It's a draw, ${userRPS} & ${computerRPS} can't beat each other`);
+      console.log(`It's a draw, ${humanSelection} & ${computerSelection} can't beat each other`);
+    }else {
+      computerScore ++;
+      console.log(`You lose...${humanSelection} is beaten by ${computerSelection}`);
     }
 
-    console.log("scoreboard:");
+    console.log("Scoreboard:");
     console.log(`You:${humanScore} Computer:${computerScore} Draw:${draw}`);
     console.log("========+========+========");
-
-    function computerChoiceConvert(){
-      switch(computerSelection){
-        case 1:
-          return "rock";
-        case 2:
-          return "paper";
-        case 3:
-          return "scissors"
-      }
-    }
   }
 }
